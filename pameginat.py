13b13928fdd15ed73bbab1467a723182b68b94eb
IlvaJuhnevica <99957068+IlvaJuhnevica@users.noreply.github.com>
2022-04-26 20:04:37 +0300

    Update pameginat.py

--- pameginat.py
+++ pameginat.py
@@ -1,14 +1,16 @@
-sk = [] 
-cipari = open("cipari.txt", "r")
-for r in cipari:
-    sk.append(r)
-    print(r, end="")
-print()
-sum = 0
-for i in range(len(sk)):
-    k = int(sk[i]) 
-    sum += k    
-print("\n Summa = ", sum)
-cipari = open("cipari.txt", "w", encoding="UTF-8")
-cipari.write(f"Summa ir {str(sum)}")
-cipari.close()
+# Fibonacci numbers module
+
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):   # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
