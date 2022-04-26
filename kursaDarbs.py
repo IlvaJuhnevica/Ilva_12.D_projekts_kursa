970a5eec74e2eacad60441a0d654c9d667e3f6b6
IlvaJuhnevica <99957068+IlvaJuhnevica@users.noreply.github.com>
2022-04-26 19:52:52 +0300

    Update kursaDarbs.py

--- kursaDarbs.py
+++ kursaDarbs.py
@@ -56,75 +56,6 @@ def is_win(player, opponent):
 print(play())
 
 print("VĀRDU MINĒŠANAS SPĒLE! UZMINI VĀRDU< KURŠ IR SAISTĪTS AR PROGRAMMĒŠANU!")
-words = ['matemātika', 'dators', 'fails', 'programmēšana',
-		'python', 'lietotājs', 'moduļi', 'masīvs',
-		'elif', 'funkcijas', 'print', 'for']
-
-word = random.choice(words)
-
-print("Guess the characters")
-
-guesses = ''
-
-# any number of turns can be used here
-turns = 12
-
-while turns > 0:
-	
-	# counts the number of times a user fails
-	failed = 0
-	
-	# all characters from the input
-	# word taking one at a time.
-	for char in word:
-		
-		# comparing that character with
-		# the character in guesses
-		if char in guesses:
-			print(char, end=" ")
-			
-		else:
-			print("_")
-			print(char, end=" ")
-			
-			# for every failure 1 will be
-			# incremented in failure
-			failed += 1
-			
-
-	if failed == 0:
-		# user will win the game if failure is 0
-		# and 'You Win' will be given as output
-		print("You Win")
-		
-		# this print the correct word
-		print("The word is: ", word)
-		break
-	
-	# if user has input the wrong alphabet then
-	# it will ask user to enter another alphabet
-	print()
-	guess = input("guess a character:")
-	
-	# every input character will be stored in guesses
-	guesses += guess
-	
-	# check input with the character in word
-	if guess not in word:
-		
-		turns -= 1
-		
-		# if the character doesn’t match the word
-		# then “Wrong” will be given as output
-		print("Wrong")
-		
-		# this will print the number of
-		# turns left for the user
-		print("You have", + turns, 'more guesses')
-		
-		
-		if turns == 0:
-			print("You Loose")
 
 def trijsturis(b):
     a=(2*b)-2
@@ -133,7 +64,7 @@ def trijsturis(b):
             print(end="")
         a=a+1
         for j in range (0, i+1):
-            print("* ",end="")
+            print( "* ",end="")
         print("\r")
 b=int(input("Ievadi līniju skaitu: "))
 trijsturis(b)
